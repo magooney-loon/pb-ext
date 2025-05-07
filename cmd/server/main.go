02824494831c04741fde033b9d0681aaeed43ddd
malooney-goon <xxzirrgrizzxx@yahoo.com>
2025-05-07 12:45:08 +0200

    db init

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -8,6 +8,7 @@ import (
 
 	app "github.com/magooney-loon/pb-ext/core"
 	"github.com/pocketbase/pocketbase/core"
+	"github.com/pocketbase/pocketbase/tools/types"
 )
 
 func main() {
@@ -18,6 +19,8 @@ func initApp() {
 	srv := app.New()
 
 	app.SetupLogging(srv)
+
+	registerCollections(srv.App())
 
 	srv.App().OnServe().BindFunc(func(e *core.ServeEvent) error {
 		app.SetupRecovery(srv.App(), e)
@@ -38,6 +41,15 @@ func initApp() {
 	}
 }
 
+func registerCollections(app core.App) {
+	app.OnServe().BindFunc(func(e *core.ServeEvent) error {
+		if err := exampleCollection(e.App); err != nil {
+			app.Logger().Error("Failed to create example collection", "error", err)
+		}
+		return e.Next()
+	})
+}
+
 func registerRoutes(app core.App) {
 	app.OnServe().BindFunc(func(e *core.ServeEvent) error {
 		e.Router.GET("/api/time", func(c *core.RequestEvent) error {
@@ -52,6 +64,72 @@ func registerRoutes(app core.App) {
 			})
 		})
 
+		// You can use POST /api/collections/users/records to create a new user
+		// See PocketBase documentation for more details: https://pocketbase.io/docs/api-records/
+
 		return e.Next()
 	})
+}
+
+func exampleCollection(app core.App) error {
+	// Example: Create a simple collection
+	existingCollection, _ := app.FindCollectionByNameOrId("example_collection")
+	if existingCollection != nil {
+		app.Logger().Info("Example collection already exists")
+		return nil
+	}
+
+	// Create new collection
+	collection := core.NewBaseCollection("example_collection")
+
+	// Find users collection for relation
+	usersCollection, err := app.FindCollectionByNameOrId("users")
+	if err != nil {
+		return err
+	}
+
+	// Add relation field to user FIRST
+	collection.Fields.Add(&core.RelationField{
+		Name:          "user",
+		Required:      true,
+		CollectionId:  usersCollection.Id,
+		CascadeDelete: true,
+	})
+
+	// Set collection rules AFTER adding the relation field
+	collection.ViewRule = types.Pointer("@request.auth.id != ''")
+	collection.CreateRule = types.Pointer("@request.auth.id != ''")
+	collection.UpdateRule = types.Pointer("@request.auth.id = user.id")
+	collection.DeleteRule = types.Pointer("@request.auth.id = user.id")
+
+	// Add other fields to collection
+	collection.Fields.Add(&core.TextField{
+		Name:     "title",
+		Required: true,
+		Max:      100,
+	})
+
+	// Add auto-date fields
+	collection.Fields.Add(&core.AutodateField{
+		Name:     "created",
+		OnCreate: true,
+	})
+
+	collection.Fields.Add(&core.AutodateField{
+		Name:     "updated",
+		OnCreate: true,
+		OnUpdate: true,
+	})
+
+	// Add index for user relation
+	collection.AddIndex("idx_example_user", true, "user", "")
+
+	// Save the collection
+	if err := app.Save(collection); err != nil {
+		app.Logger().Error("Failed to create example collection", "error", err)
+		return err
+	}
+
+	app.Logger().Info("Created example collection")
+	return nil
 }
