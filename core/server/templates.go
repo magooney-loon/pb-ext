28035b856addd41ebcbc323be448796d2f00d651
Magooney <xxzirrgrizzxx@yahoo.com>
2025-10-06 10:07:48 +0200

    basic api tester

--- core/server/templates.go
+++ core/server/templates.go
@@ -4,5 +4,5 @@ import (
 	"embed"
 )
 
-//go:embed templates/*.tmpl templates/components/*.tmpl templates/scripts/*.tmpl templates/css/*.tmpl
+//go:embed templates/*.tmpl templates/components/*.tmpl templates/scripts/*.tmpl templates/scripts/api/*.tmpl templates/css/*.tmpl
 var TemplateFS embed.FS
